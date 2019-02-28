213c7dfbdf2e63af7bcd51be900e38b21691edd1
= <=>
2019-02-28 10:54:51 -0300

    kaka

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/Header';
+import NewUser from './components/NewUser';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div>
-          <h1 class="title">😎✌ <br/> Luciano Sizilio</h1>
-        </div>
+      <div>
+        <Header />
+        <NewUser />
       </div>
     );
   }
